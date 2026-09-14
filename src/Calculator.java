e4e96390500f001a0c9a289430fe54e8d3096a62
Andrey Lucenko <andlucenko@gmail.com>
2026-09-14 22:49:48 +0300

    feat: introduce Calculator as primary application class

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static double currentValue = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
