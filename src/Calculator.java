6aac2fa45deedd946690b8abfce49b655d1cfd1f
Andrey Lucenko <andlucenko@gmail.com>
2026-09-14 23:38:26 +0300

    feat: Implement addition operation and result output

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,16 +18,45 @@ public class Calculator {
         return scanner.next();
     }
 
+    private static void output() {
+        System.out.println("Результат: ");
+
+        if (currentValue == (long) currentValue) {
+            System.out.println((long) currentValue);
+            return;
+        }
+
+        System.out.println(currentValue);
+    }
+
     private static void reset() {
         currentValue = 0;
     }
 
+    private static void sum(double number) {
+        currentValue += number;
+    }
+
+    private static void executeOperation(String operation) {
+        switch (operation) {
+            case "+":
+                double number = toNumber(input("Введите число: "));
+                sum(number);
+                output();
+                break;
+            default:
+                System.out.println("Неизвестная операция.");
+                break;
+        }
+    }
+
     private static void run() {
+        isRunning = true;
         currentValue = toNumber(input("Введите число: "));
 
         while (isRunning) {
-            String operation = input("Введите операцию: ");
-            System.out.println("Оператор: " + operation);
+            String operation = input("Введите операцию: +");
+            executeOperation(operation);
         }
 
         scanner.close();
